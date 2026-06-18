5aae9ea2b2d8d7356c5789ceada64afaba41b80e
Kavindu Udara <155027870+kavindu-udara@users.noreply.github.com>
2026-06-18 13:37:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), eslint(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
